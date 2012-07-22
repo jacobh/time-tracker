1de6178cdc6d6e4a8c9d63023681886dd801bad6
Jacob Haslehurst <jacob@haslehurst.net>
2012-07-22 17:21:11 +0800

    Implemented gravatar's for user
    gravatar method on user which hashes the user's email address and then
    puts it on the end of the avatar base url, with an optional size
    argument.

--- models/user.rb
+++ models/user.rb
@@ -1,7 +1,14 @@
+require "Digest"
+
 class User < ActiveRecord::Base
-    attr_accessible :email, :name, :password
-    has_secure_password
-    has_many :slips
-    has_and_belongs_to_many :projects
-    validates :email, uniqueness: true
+  attr_accessible :email, :name, :password
+  has_secure_password
+  has_many :slips
+  has_and_belongs_to_many :projects
+  validates :email, uniqueness: true
+
+  def gravatar_url(size)
+    hex = Digest::MD5.new.hexdigest email.downcase
+    "http://www.gravatar.com/avatar/#{hex}?=#{size}"
+  end
 end

bce11650ab46f893323020f34918af77d20e92bb
Jacob Haslehurst <jacob@haslehurst.net>
2012-07-22 16:46:11 +0800

    currently not working implementation of user signup

--- app.rb
+++ app.rb
@@ -10,6 +10,12 @@ ActiveRecord::Base.establish_connection CONFIG["db"]
 set :erb, :escape_html => true
 enable :sessions
 
+before do
+  if session[:user_id]
+    @current_user = User.find(session[:user_id])
+  end
+end
+
 get "/" do
   erb :index
 end
@@ -19,12 +25,26 @@ get "/login" do
 end
 
 post "/login" do
-  user = User.find_by_email params[:email]
-  if user and user.authenticate [:password]
-    session[:user_id] = user.id
+  @user = User.find_by_email params[:email]
+  if @user and user.authenticate [:password]
+    session[:user_id] = @user.id
     "now logged in successfully"
   else
     "something went wrong while trying to log you in"
+  end
+end
+
+get "/signup" do
+  erb :'user/signup'
+end
+
+post "/signup" do
+  @user = User.new params
+  if @user.save
+    session[:user_id] = @user.id
+    "successfully signed you up!"
+  else
+    "uh oh stuff went wrong"
   end
 end
 
--- models/user.rb
+++ models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
-    attr_accessible :email, :name
+    attr_accessible :email, :name, :password_digest
     has_secure_password
     has_many :slips
     has_and_belongs_to_many :projects
+    validates :email, uniqueness: true
 end

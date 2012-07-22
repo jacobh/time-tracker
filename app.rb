231d7bcebd9051933378eb06823658bde0cdc334
Jacob Haslehurst <jacob@haslehurst.net>
2012-07-22 14:51:19 +0800

    implemented user login

--- app.rb
+++ app.rb
@@ -8,9 +8,24 @@ CONFIG = YAML.load_file("config.yml")[Sinatra::Application.environment.to_s]
 ActiveRecord::Base.establish_connection CONFIG["db"]
 
 set :erb, :escape_html => true
+enable :sessions
 
 get "/" do
   erb :index
+end
+
+get "/login" do
+  erb :'user/login'
+end
+
+post "/login" do
+  user = User.find_by_email params[:email]
+  if user and user.authenticate [:password]
+    session[:user_id] = user.id
+    "now logged in successfully"
+  else
+    "something went wrong while trying to log you in"
+  end
 end
 
 get "/style.css" do
